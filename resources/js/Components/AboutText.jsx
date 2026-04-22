c7e6f9ce8e3c96889fd83dcb053b39dceb059fa4
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-22 17:37:56 -0300

    refactor: remove animação de imagem direita e melhora a responsividade do AboutText

--- resources/js/Components/AboutText.jsx
+++ resources/js/Components/AboutText.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -11,10 +11,9 @@ gsap.registerPlugin(ScrollTrigger);
 export const AboutText = ({ content, video }) => {
     const bgRef = useRef(null);
     const leftImageRef = useRef(null);
-    const rightImageRef = useRef(null);
 
     useEffect(() => { 
-        if (!bgRef.current || !leftImageRef.current || !rightImageRef.current) return;
+        if (!bgRef.current || !leftImageRef.current ) return;
 
         gsap.fromTo(bgRef.current, 
         {
@@ -61,19 +60,7 @@ export const AboutText = ({ content, video }) => {
             repeat: -1,
             yoyo: true,
             ease: "sine.inOut",
-        });  
-
-        gsap.fromTo(rightImageRef.current, 
-        {
-            rotation: -150
-        },
-        {
-            rotation: -160,
-            duration: 2,
-            repeat: -1,
-            yoyo: true,
-            ease: "sine.inOut",
-        });        
+        });         
     }, []);
 
     const partesTitulo = content.texto ? content.titulo.split(",") : [];
@@ -82,7 +69,7 @@ export const AboutText = ({ content, video }) => {
     return (
         <section
             ref={bgRef}
-            className="relative w-full max-[430px]:bg-[length:auto_120%] max-[570px]:bg-[length:200%] sm:bg-[length:170%] bg-[60%] xl:bg-[length:100%]"
+            className="relative w-full sm:bg-[length:170%] bg-[60%] xl:bg-[length:100%]"
             style={{
                 backgroundImage: `url(${content.imagem})`,
             }}
@@ -91,37 +78,33 @@ export const AboutText = ({ content, video }) => {
                 <div className="container max-w-medium">
                     <div className="py-24">
                         {partesTitulo.map((parte, i) => (
-                            <LetterReveal key={i} className="font-secondary text-primary text-center text-7xl font-black leading-[1.2] uppercase -mt-2" text={parte.trim()} element="h1" />
+                            <LetterReveal key={i} className="font-secondary text-primary text-center text-5xl md:text-6xl 2xl:text-7xl font-black leading-[1.2] -mt-2" text={parte.trim()} element="h1" />
                         ))}
                         
-                        <div className="text-2xl text-center [&_p_+_p]:mt-0 mt-4">
+                        <div className="text-xl 2xl:text-2xl text-center [&_p_+_p]:mt-0 mt-4">
                             {partesSubtitulo.map((parte, i) => (
                                 <p key={i} className={i !== 0 ? 'font-bold' : ''}>{parte}</p>
                             ))}
                         </div>
 
-                        <div className="text-neutral-600 text-center mt-12 pt-12 border-t" dangerouslySetInnerHTML={{ __html: content.texto }} /> 
+                        <div className="text-xs md:text-sm 2xl:text-base text-gray-800 sm:text-custom-gray text-center mt-10 2xl:mt-12 2xl:pt-12" dangerouslySetInnerHTML={{ __html: content.texto }} /> 
                     </div>
                 </div>
             </div>
 
-            <div className="mt-20 -mb-[20%]">
+            <div className="mt-1 2xl:mt-20 -mb-[20%]">
                 <div className="container max-w-large">
-                    <VideoPlayer src={video} classList={['sm:max-w-[78vw]', 'mx-auto', 'w-full', 'aspect-video']} autoplay={false} />
+                    <VideoPlayer src={video} classList={['sm:max-w-[78vw]', 'mx-auto', 'w-full', 'aspect-video', 'rounded-t-2xl']} autoplay={false} />
                 </div>
             </div>
 
             <img
                 ref={leftImageRef}
                 src={content.imagem_mobile}
-                className="absolute w-80 -left-20 drop-shadow-lg z-[1]"
+                className="absolute w-40 md:w-50 2xl:w-80 -left-20 drop-shadow-lg z-[1]"
             />
 
-            <img
-                ref={rightImageRef}
-                src={content.imagem_mobile}
-                className="absolute -right-20 -bottom-[10%] drop-shadow-lg z-[2]"
-            />
+            <div className='bg-primary pt-[20%]'/>
         </section>
     );
-};
+}; 
