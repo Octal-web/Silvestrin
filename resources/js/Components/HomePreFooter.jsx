220e401e9b6fafb5e8b129d9bc794bb981ba3c28
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-22 17:50:45 -0300

    style: altera responsividade do HomePreFooter

--- resources/js/Components/HomePreFooter.jsx
+++ resources/js/Components/HomePreFooter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -32,16 +32,16 @@ export const HomePreFooter = ({ content }) => {
     return (
         <section
             ref={bgRef}
-            className="relative w-full h-[422px] max-[430px]:bg-[length:auto_120%] max-[570px]:bg-[length:200%] sm:bg-[length:170%] bg-[60%] xl:bg-[length:100%]"
+            className="relative w-full h-[300px] 2xl:h-[422px] max-[430px]:bg-[length:auto_120%] max-[570px]:bg-[length:200%] sm:bg-[length:170%] bg-[60%] xl:bg-[length:100%] rounded-t-3xl"
             style={{
                 backgroundImage: `url(${content.imagem})`,
             }}
         >
-            <div className="absolute top-0 left-0 bottom-0 w-2/3 bg-gradient-to-r from-black to-transparent opacity-50" />
+            <div className="absolute top-0 left-0 bottom-0 w-2/3 bg-gradient-to-r from-black to-transparent opacity-50 rounded-t-3xl" />
             <div className="container max-w-large h-full">
                 <div className="flex h-full justify-center flex-col">
                     {linhas.map((linha, index) => (
-                        <LetterReveal key={index} className="font-secondary text-white text-5xl font-black leading-tight uppercase" text={linha} element="h2" />
+                        <LetterReveal key={index} className="font-secondary text-white text-3xl md:text-4xl 2xl:text-5xl font-black leading-tight" text={linha} element="h2" />
                     ))}
                 </div>
             </div>
