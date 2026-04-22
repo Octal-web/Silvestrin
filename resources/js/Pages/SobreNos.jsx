76483ef3ad78e02fbc7160b94e578d773e947e55
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-22 17:36:58 -0300

    refactor: remove componentes AboutESG, AboutRain, AboutStation e AboutWorkWith. Adiciona o componente AboutOperation

--- resources/js/Pages/SobreNos.jsx
+++ resources/js/Pages/SobreNos.jsx
@@ -2,26 +2,27 @@ import { usePage } from '@inertiajs/react';
 
 import DefaultLayout from '@/Layouts/DefaultLayout';
 
+import { AboutOperation } from '@/Components/AboutOperation';
 import { AboutText } from '@/Components/AboutText';
-import { AboutESG } from '@/Components/AboutESG';
-import { AboutRain } from '@/Components/AboutRain';
-import { AboutStation } from '@/Components/AboutStation';
-import { AboutWorkWith } from '@/Components/AboutWorkWith';
 
 const Page = () => {
     const { tradicaoVideo, conteudos } = usePage().props;
 
+    console.log(conteudos)
+
     return (
         <DefaultLayout>
             <AboutText content={conteudos[0]} video={tradicaoVideo} />
+
+            <AboutOperation content={[conteudos[7], conteudos[6], conteudos[5]]} />
             
-            <AboutESG content={conteudos[1]} />
+            {/* <AboutESG content={conteudos[1]} />
 
             <AboutRain content={conteudos[2]} />
 
             <AboutStation content={conteudos[3]} />
 
-            <AboutWorkWith content={conteudos[4]} />
+            <AboutWorkWith content={conteudos[4]} /> */}
         </DefaultLayout>
     );
 };
