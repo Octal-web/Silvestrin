6d19b21c68b628af7849bfe3d2cf0b54a10219c4
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-22 17:43:02 -0300

    refactor: simplifica Home removendo HomeTeam, HomeCompromise, HomeRain, HomeStation e HomeContact. Adiciona o componente HomeCertification

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -2,19 +2,15 @@ import { usePage } from '@inertiajs/react';
 
 import DefaultLayout from '@/Layouts/DefaultLayout';
 
-import { HomeSlides } from '@/Components/HomeSlides';
-import { HomeValues } from '@/Components/HomeValues';
 import { HomeBrands } from '@/Components/HomeBrands';
-import { HomeSteps } from '@/Components/HomeSteps';
-import { HomeTeam } from '@/Components/HomeTeam';
-import { HomeCompromise } from '@/Components/HomeCompromise';
-import { HomeRain } from '@/Components/HomeRain';
-import { HomeStation } from '@/Components/HomeStation';
-import { HomeContact } from '@/Components/HomeContact';
+import { HomeCertification } from '@/Components/HomeCertification';
 import { HomePreFooter } from '@/Components/HomePreFooter';
+import { HomeSlides } from '@/Components/HomeSlides';
+import { HomeSteps } from '@/Components/HomeSteps';
+import { HomeValues } from '@/Components/HomeValues';
 
 const Page = () => {
-    const { slides, valores, marcas, tradicaoVideo, conteudos } = usePage().props;
+    const { slides, valores, marcas, tradicaoVideo, conteudos, certificacoes } = usePage().props;
 
     return (
         <DefaultLayout>
@@ -26,15 +22,17 @@ const Page = () => {
 
             <HomeSteps content={conteudos[1]} steps={[conteudos[2], conteudos[3], conteudos[4], conteudos[5]]} video={tradicaoVideo} />
 
-            <HomeTeam content={conteudos[6]} />
+            <HomeCertification certificacoes={certificacoes[0]}/>
 
-            <HomeCompromise content={conteudos[7]} />
-            
-            <HomeRain content={conteudos[8]} />
+            {/* <HomeTeam content={conteudos[6]} /> */}
 
-            <HomeStation content={conteudos[9]} />
+            {/* <HomeCompromise content={conteudos[7]} /> */}
             
-            <HomeContact isHome={true} content={conteudos[10]} />
+            {/* <HomeRain content={conteudos[8]} /> */}
+
+            {/* <HomeStation content={conteudos[9]} /> */}
+            
+            {/* <HomeContact isHome={true} content={conteudos[10]} /> */}
 
             <HomePreFooter content={conteudos[11]} />
         </DefaultLayout>
