26d9d5046dbc44c1d44cecde059768d33a2c2042
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-24 09:29:31 -0300

    feat: altera TransportBanner com novo layout e estrutura de título

--- resources/js/Components/TransportBanner.jsx
+++ resources/js/Components/TransportBanner.jsx
@@ -1,46 +1,35 @@
-import React, { useEffect, useRef } from 'react';
 
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import LetterReveal from './LetterReveal';
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import LetterReveal from "./LetterReveal";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export const TransportBanner = ({ content }) => {
-    const bgRef = useRef(null);
 
-    useEffect(() => { 
-        gsap.fromTo(bgRef.current, 
-        {
-            backgroundPositionY: '100%',
-        },
-        {
-            backgroundPositionY: '0%',
-            duration: 1,
-            ease: 'none',
-            scrollTrigger: {
-                trigger: bgRef.current,
-                start: 'top bottom',
-                end: 'bottom top',
-                scrub: true
-            }
-        });
-    }, []);
+    const partesTitulo = content.texto ? content.titulo.split(",") : [];
 
-    
     return (
         <section
-            ref={bgRef}
-            className="relative w-full h-[570px] max-[430px]:bg-[length:auto_120%] max-[570px]:bg-[length:200%] sm:bg-[length:170%] bg-[60%] xl:bg-[length:100%]"
+            className="relative bg-no-repeat bg-fill 2xl:bg-cover min-w-screen h-screen max-h-[672px] -mt-16 md:-mt-10"
             style={{
                 backgroundImage: `url(${content.imagem})`,
             }}
         >
-            <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50" />
-            <div className="container max-w-large h-full">
-                <div className="flex h-full justify-center flex-col">
-                    <LetterReveal className="font-secondary text-white text-7xl text-center font-black uppercase mb-32" text={content.titulo} element="h1" />
-                </div>
+            <div className="container max-w-large flex flex-col justify-center h-full pb-10">
+                {partesTitulo.map((parte, i) => (
+                    <LetterReveal
+                        key={i}
+                        className={`font-secondary ${i === 0 ? "text-primary" : "text-secondary"} text-center text-5xl md:text-6xl 2xl:text-7xl font-black leading-[1.2] -mt-2`}
+                        text={parte.trim()}
+                        element="h1"
+                    />
+                ))}
+
+                <div
+                    className="text-xs md:text-sm 2xl:text-base text-custom-gray text-center mt-10 2xl:mt-12 2xl:pt-12 max-w-3xl mx-auto"
+                    dangerouslySetInnerHTML={{ __html: content.texto }}
+                />
             </div>
         </section>
     );
