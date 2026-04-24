c27a3e37048777a0f46acb3cb078b04b6452dfa6
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-24 17:54:40 -0300

    refactor: altera estrutura do layout da página produtos

--- resources/js/Pages/Produtos.jsx
+++ resources/js/Pages/Produtos.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { usePage, Link, router } from '@inertiajs/react';
+import { router, usePage } from "@inertiajs/react";
+import { useEffect, useState } from "react";
 
-import DefaultLayout from '@/Layouts/DefaultLayout';
+import DefaultLayout from "@/Layouts/DefaultLayout";
 
-import { ProductsBanner } from '@/Components/ProductsBanner';
-import { ProductsCategories } from '@/Components/ProductsCategories';
-import { ProductsList } from '@/Components/ProductsList';
+import { ProductsBanner } from "@/Components/ProductsBanner";
+import { ProductsCategories } from "@/Components/ProductsCategories";
+import { ProductsList } from "@/Components/ProductsList";
 
 const Page = () => {
     const { conteudos, marca, todasMarcas, todasCategorias } = usePage().props;
@@ -18,46 +18,35 @@ const Page = () => {
 
             const url = window.location.search;
             const urlParams = new URLSearchParams(url);
-            const marcaUrl = urlParams.get('marca');
+            const marcaUrl = urlParams.get("marca");
 
             const params = {};
             if (marcaUrl) params.marca = marcaUrl;
 
-                router.get(
-                    window.location.href,
-                    params,
-                    {
-                        preserveState: true,
-                        preserveScroll: true,
-                        only: ['marca', 'produtos'],
-                        onSuccess: () => {
-                            setLoading(false);
-                            setHasChanged(false);
-                        },
-                    },
-            );
+            router.get(window.location.href, params, {
+                preserveState: true,
+                preserveScroll: true,
+                only: ["marca", "produtos"],
+                onSuccess: () => {
+                    setLoading(false);
+                    setHasChanged(false);
+                },
+            });
         }
     }, [hasChanged]);
 
     return (
         <DefaultLayout>
             <ProductsBanner content={conteudos[0]} />
-            <section className="relative mt-10 md:mt-20 mb-20 md:mb-30">
-                {!loading && (
-                    <div className="absolute right-0 top-0 -bottom-30 w-[70.5%] bg-neutral-100 rounded-t-3xl" />
-                )}
-                
-                <div className="container max-w-large">
-                    <div className="md:flex">
-                        <ProductsCategories 
-                            brands={todasMarcas}
-                            categories={todasCategorias}
-                            hasChanged={setHasChanged}
-                        />
-                        <ProductsList hasAside={true} brand={marca} loading={loading} />
-                    </div>
-                </div>
-            </section>
+
+            <div className="md:flex">
+                <ProductsCategories
+                    brands={todasMarcas}
+                    categories={todasCategorias}
+                    hasChanged={setHasChanged}
+                />
+                <ProductsList hasAside={true} brand={marca} loading={loading} />
+            </div>
         </DefaultLayout>
     );
 };
