91743c2b1e64bc157126f2830479ebfb3d01609f
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-28 17:50:02 -0300

    style: altera responsividade do componente TransportRoutes

--- resources/js/Components/TransportRoutes.jsx
+++ resources/js/Components/TransportRoutes.jsx
@@ -53,7 +53,7 @@ export const TransportRoutes = ({ content }) => {
     return (
         <section
             ref={sectionRef}
-            className="relative bg-primary text-white pb-10 md:pb-0 min-w-screen"
+            className="relative bg-primary text-white pb-10 md:pb-0"
         >
             <img
                 ref={imageRef}
@@ -61,7 +61,10 @@ export const TransportRoutes = ({ content }) => {
                 className="absolute max-h-[10%] md:max-h-[26%] 2xl:max-h-[78%] -left-[3rem] md:-left-40 drop-shadow-lg"
                 style={{ top: "0%" }}
             />
-            <div className="flex flex-col-reverse gap-10 md:gap-10 md:flex-row md:justify-between items-center container max-w-medium min-h-[586px]">
+            <div
+                className="flex flex-col-reverse gap-10 2xl:gap-[85px]
+             md:flex-row md:justify-start items-center container max-w-large min-h-[586px]"
+            >
                 <Reveal direction="left">
                     <LetterReveal
                         className="font-secondary text-3xl md:text-4xl 2xl:text-5xl font-black leading-tight md:max-w-[303px] text-center md:text-start "
@@ -76,9 +79,12 @@ export const TransportRoutes = ({ content }) => {
                     />
                 </Reveal>
 
-                <Reveal direction="right" className="self-center md:self-end">
+                <Reveal
+                    direction="right"
+                    className="self-center md:self-end pt-2 md:pt-0"
+                >
                     <img
-                        className="h-auto max-h-[300px] md:max-h-full rounded-3xl md:h-[508px] md:rounded-t-3xl md:rounded-b-none object-cover"
+                        className="w-full 2xl:w-[740px] rounded-3xl md:rounded-t-3xl md:rounded-b-none object-cover max-w-[570px] xl:max-w-[630px] 2xl:max-w-[735px] h-[300px] md:h-[508px]"
                         src={content.imagem}
                         alt={content.titulo}
                     />
