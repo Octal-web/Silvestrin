069d57932c01eefd236697c22b290b37802d0eb7
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-30 15:51:49 -0300

    feat: adiciona sustentabilidade método com listagem de parcerias ao InstitucionalController

--- app/Http/Controllers/InstitucionalController.php
+++ app/Http/Controllers/InstitucionalController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Parceria;
 use Inertia\Inertia;
 
 class InstitucionalController extends Controller
@@ -12,13 +12,50 @@ class InstitucionalController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
-        $idioma = inertia()->getShared('idioma');
-        
+    public function index()
+    {
         $tradicaoVideo = rafator('content/files/' . inertia()->getShared('conteudos')[0]['arquivo']);
 
         return Inertia::render('SobreNos', [
             'tradicaoVideo' => $tradicaoVideo
+        ]);
+    }
+
+    public function sustentabilidade()
+    {
+        $idioma = inertia()->getShared('idioma');
+
+        $todasParcerias = Parceria::query()
+            ->where([
+                'excluido' => NULL,
+                'visivel' => true
+            ])
+            ->with([
+                'parceriasIdiomas' => function ($q) use ($idioma) {
+                    $q->whereHas('idiomas', function ($r) use ($idioma) {
+                        $r->where('codigo', $idioma)
+                            ->orWhere('padrao', true);
+                    })
+                        ->orderBy('idioma_id', 'DESC');
+                }
+            ])
+            ->orderBy('ordem', 'ASC')
+            ->orderBy('id', 'DESC')
+            ->get()
+            ->map(function ($parceria) {
+                return [
+                    'id' => $parceria->id,
+                    'logo' => $parceria->logo ? rafator('content/partnerships/' . $parceria->logo) : null,
+                    'imagem' => rafator('content/display/' . $parceria->imagem),
+                    'titulo' => $parceria->parceriasIdiomas->isNotEmpty() ? $parceria->parceriasIdiomas[0]->titulo : null,
+                    'subtitulo' => $parceria->parceriasIdiomas->isNotEmpty() ? $parceria->parceriasIdiomas[0]->subtitulo : null,
+                    'texto' => $parceria->parceriasIdiomas->isNotEmpty() ? $parceria->parceriasIdiomas[0]->texto : null,
+                    'cor' => $parceria->parceriasIdiomas->isNotEmpty() ? $parceria->parceriasIdiomas[0]->cor : null,
+                ];
+            });
+
+        return Inertia::render('Sustentabilidade', [
+            'todasParcerias' => $todasParcerias,
         ]);
     }
 };
