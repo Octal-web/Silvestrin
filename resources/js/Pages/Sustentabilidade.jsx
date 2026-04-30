c6bb358577b809d6c884331236e9968961810b9c
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-30 15:59:19 -0300

    refactor: renomeia componente na Sustentabilidade page

--- resources/js/Pages/Sustentabilidade.jsx
+++ resources/js/Pages/Sustentabilidade.jsx
@@ -3,8 +3,8 @@ import { usePage } from "@inertiajs/react";
 import { SustainabilityBanner } from "@/Components/SustainabilityBanner";
 import { SustainabilityCommitment } from "@/Components/SustainabilityCommitment";
 import { SustainabilityPartnerships } from "@/Components/SustainabilityPartnerships";
+import { SustainabilityProject } from "@/Components/SustainabilityProject";
 import { SustainabilityReuse } from "@/Components/SustainabilityReuse";
-import { SustainabilitySustainable } from "@/Components/SustainabilitySustainable";
 import DefaultLayout from "@/Layouts/DefaultLayout";
 
 const Page = () => {
@@ -13,7 +13,7 @@ const Page = () => {
     return (
         <DefaultLayout>
             <SustainabilityBanner content={conteudos[0]} />
-            <SustainabilitySustainable content={[conteudos[1], conteudos[2]]} />
+            <SustainabilityProject content={[conteudos[1], conteudos[2]]} />
             <SustainabilityReuse content={conteudos[3]} />
             <SustainabilityCommitment content={conteudos[4]} />
             <SustainabilityPartnerships
