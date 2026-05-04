6c21962cfd0dd71cc4c38a72dc0e4892a0556a88
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 17:36:30 -0300

    feat: adiciona requests para slide e valor

---
+++ app/Http/Requests/Manager/ValorRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Manager;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ValorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules()
+    {
+        return [
+            'nome'  => 'required',
+            'texto'  => 'required',
+            'icone' => (inertia()->getShared('action') === 'novo'
+                ? 'required|image|mimes:png,jpg|max:2048'
+                : 'nullable|image|mimes:png,jpg|max:2048'),
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return [
+            'nome.required'  => 'Por favor, informe o título.',
+            'texto.required'  => 'Por favor, informe a descrição.',
+            'icone.required' => 'Por favor, selecione uma imagem.',
+            'icone.image' => 'Por favor, selecione uma imagem válida.',
+            'icone.mimes' => 'Os formatos de imagem válidos são: JPG e PNG.',
+            'icone.max' => 'Por favor, envie um arquivo menor que 2MB.',
+        ];
+    }
+}
