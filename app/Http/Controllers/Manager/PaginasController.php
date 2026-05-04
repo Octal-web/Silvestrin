f77c2bfb3481b01043280ad6c8a3ed4386c33ea2
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-04 17:35:18 -0300

    feat: adiciona PaginasController para Manager

---
+++ app/Http/Controllers/Manager/PaginasController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Manager;
+
+use App\Http\Controllers\Controller;
+use App\Services\PaginasService;
+use Illuminate\Http\Request;
+
+class PaginasController extends Controller
+{
+    protected $service;
+
+    public function __construct(PaginasService $service)
+    {
+        parent::__construct();
+        $this->service = $service;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function editarAction(Request $request, $id)
+    {
+        if ($request->ajax()) {
+            $response = $this->service->editarPagina($request, $id);
+
+            return $response
+                ? to_route('Manager.Home.index')->with('message', ['type' => 'success', 'msg' => 'Registro salvo com sucesso!'])
+                : to_route('Manager.Home.index')->with('message', ['type' => 'error', 'msg' => 'Não foi possível salvar as informações.']);
+        }
+
+        return to_route('Manager.Home.index')->with('message', ['type' => 'error', 'msg' => 'Não foi possível salvar as informações.']);
+    }
+}
