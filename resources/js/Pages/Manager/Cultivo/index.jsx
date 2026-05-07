d0a2ec4e014a91be20b977e5d28f7bb114f06c43
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-07 17:45:45 -0300

    feat: adiciona no manager Cultivo página

---
+++ resources/js/Pages/Manager/Cultivo/index.jsx
@@ -0,0 +1,82 @@
+import { usePage } from "@inertiajs/react";
+
+import { Breadcrumb } from "@/Components/Manager/Breadcrumb";
+import { FormContent } from "@/Components/Manager/FormContent";
+import AdminLayout from "@/Layouts/AdminLayout";
+import { faTruck } from "@fortawesome/free-solid-svg-icons";
+
+const Page = () => {
+    const { conteudos, idioma, idiomas } = usePage().props;
+
+    const breadcrumbItems = [
+        // { label: 'Home', link: 'Home.index' },
+        // { label: 'Projects', link: 'Home.index' },
+    ];
+
+    console.log(conteudos);
+
+    return (
+        <AdminLayout>
+            <Breadcrumb
+                icon={faTruck}
+                items={breadcrumbItems}
+                current="Cultivo"
+                idioma={idioma.codigo}
+                idiomas={idiomas}
+            />
+
+            <FormContent
+                content={conteudos[1]}
+                full={true}
+                idioma={idioma.codigo}
+            />
+
+            <FormContent
+                content={conteudos[2]}
+                full={true}
+                idioma={idioma.codigo}
+            />
+
+            <FormContent
+                content={conteudos[3]}
+                full={true}
+                idioma={idioma.codigo}
+            />
+
+            <FormContent
+                content={conteudos[4]}
+                full={true}
+                idioma={idioma.codigo}
+            />
+
+            <FormContent
+                content={conteudos[5]}
+                full={true}
+                idioma={idioma.codigo}
+            />
+
+            <div className="grid lg:gap-x-4 lg:grid-cols-2">
+                <FormContent
+                    content={conteudos[6]}
+                    full={false}
+                    idioma={idioma.codigo}
+                />
+
+                <FormContent
+                    content={conteudos[7]}
+                    full={false}
+                    toolbar={["UlList"]}
+                    idioma={idioma.codigo}
+                />
+            </div>
+
+            <FormContent
+                content={conteudos[0]}
+                full={true}
+                idioma={idioma.codigo}
+            />
+        </AdminLayout>
+    );
+};
+
+export default Page;
