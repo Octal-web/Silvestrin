eab786ec4fb01d43896eb4eb6df8891e4d641627
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-07 17:37:30 -0300

    refactor: altera services para pasta Manager

--- app/Services/Manager/ConteudosService.php
+++ app/Services/Manager/ConteudosService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Manager;
 
 use App\Models\Conteudo;
 use App\Models\ConteudoIdioma;
--- app/Services/Manager/ParceriasService.php
+++ app/Services/Manager/ParceriasService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\Manager;
 
 use App\Models\Idioma;
 use App\Models\Parceria;
