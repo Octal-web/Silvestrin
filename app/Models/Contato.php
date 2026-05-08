f48ca5060079e5345dc12f17a1571d97a298a6c4
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-08 17:47:35 -0300

    fix: remove guarded duplicado do Contato model

--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -8,13 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Contato extends Model
 {
     protected $table = 'contatos';
-    
+
     protected $guarded = ['id'];
-    
+
     const CREATED_AT = 'criado';
     const UPDATED_AT = 'modificado';
 
     protected $fillable = ['*'];
-
-    protected $guarded = ['id'];
 }
