1d527bbe5e41ccf2352509257fb7d79f7fba9b13
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-11 17:48:48 -0300

    feat: adiciona traduções para mensagens de erro do form para contato

---
+++ resources/lang/en/default.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'nome.required' => 'Please enter your name.',
+    'email.required' => 'Please enter your email address.',
+    'email.email' => 'Please enter a valid email address.',
+    'telefone.required' => 'Please enter your phone number.',
+    'telefone.celular_com_ddd' => 'Please provide a valid phone number.',
+    'mensagem.required'  => 'Please state your message.',
+    'politica.required' => 'To continue, you must agree to the privacy policy.',
+    'politica.accepted' => 'To continue, you must agree to the privacy policy.',
+    'assunto.required'  => 'Please state your subject.',
+    'area.required'  => 'Please specify your subject area.',
+];
---
+++ resources/lang/pt/default.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'nome.required' => 'Por favor, insira seu nome.',
+    'email.required' => 'Por favor, insira seu e-mail.',
+    'email.email' => 'Por favor, insira um e-mail válido.',
+    'telefone.required' => 'Por favor, insira seu telefone.',
+    'telefone.celular_com_ddd' => 'Por favor, informe um telefone válido.',
+    'mensagem.required'  => 'Por favor, informe a sua mensagem.',
+    'politica.required' => 'Para continuar, você deve concordar com a LGPD.',
+    'politica.accepted' => 'Para continuar, você deve concordar com a LGPD.',
+    'assunto.required'  => 'Por favor, informe o seu assunto.',
+    'area.required'  => 'Por favor, informe a sua área assunto.',
+];
