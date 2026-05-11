16278a4899ebf08d1266fdbf98af06da8660ed3d
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-11 17:55:46 -0300

    fix: desabilita página de erro customizada em ambiente local

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -32,6 +32,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
             if (
+                !app()->environment(['local']) &&
                 in_array($response->getStatusCode(), [403, 404, 500, 503])
             ) {
                 return Inertia::render('Erro', [
